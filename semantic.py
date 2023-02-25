81e1e38f54f918ce28c32c4035d7b79ea959ce37
bali3 <86802324+bali3@users.noreply.github.com>
2023-02-25 17:17:59 +0000

    Compulsory Task 1

---
+++ semantic.py
@@ -0,0 +1,38 @@
+#--SIMILARITY WITH SPACY--
+
+import spacy
+nlp = spacy.load('en_core_web_md')
+
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+#--WORKING WITH VECTORS--
+tokens = nlp('cat apple monkey banana ')
+for token1 in tokens:
+ 
+ for token2 in tokens:
+      print(token1.text, token2.text, token1.similarity(token2))
+
+#--WORKING WITH SENTENCES--
+sentence_to_compare = "Why is my cat on the car"
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+      similarity = nlp(sentence).similarity(model_sentence)
+      print(sentence + " - ",  similarity)
+
+
+
+#'apple and monkey' are more similar than 'apple and cat'. i guess there is more chance of a monkey eating an apple than a cat?
+
+#there is a lot less similarities using 'en_core_web_sm'.
+
